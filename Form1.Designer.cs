0fc19305d22c6feee85cff278ea9df7f7012eaff
abeny <abeny@AHMEDBENYUSUF>
2024-11-30 17:20:51 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace C07_P02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.LabelResults = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(10, 340);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 41);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(375, 340);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(75, 41);
+            this.CheckButton.TabIndex = 1;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // LabelResults
+            // 
+            this.LabelResults.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LabelResults.Location = new System.Drawing.Point(10, 9);
+            this.LabelResults.Name = "LabelResults";
+            this.LabelResults.Size = new System.Drawing.Size(440, 209);
+            this.LabelResults.TabIndex = 43;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 20);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Click the check button to grade students test";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LabelResults);
+            this.Controls.Add(this.CheckButton);
+            this.Controls.Add(this.ExitButton);
+            this.Name = "Form1";
+            this.Text = "DMV Test Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.Label LabelResults;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿//Name: Ahmed Benyusuf
+//Class and Section (CS 313 01)
+//Assignment (Program 02 Chapter 07)
+//Description of the Program: This program reads a file that contains the answers to a test and compares them to the correct answers. It then displays the results of the test.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C07_P02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadTest(List<string> TestList)
+        {
+            try
+            {
+                StreamReader inputFile = File.OpenText("Test.txt");
+                //  Open the file
+                while (!inputFile.EndOfStream)
+                //  Continue processing until the end of the file is reached
+                {
+                    string Line = inputFile.ReadLine();
+                    if (string.IsNullOrEmpty(Line))
+                    {
+                        continue;
+                    }
+                    // Add the line to the list
+                    TestList.Add(Line);
+                }
+                inputFile.Close();
+                // Close the file
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("The file 'Correct.txt' was not found.", "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Display an error message if the file is not found
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Display an error message if an exception occurs
+            }
+        }
+
+        private void CorrectTest(List<string> Answerlist)
+        {
+            //creat an array to hold the correct answers
+            string[] CorrectAnswers = { "B", "D", "A", "A", "C", "A", "B", "A", "C", "D", "B", "C", "D", "A", "D", "C", "C", "B", "D", "A" };
+            //clear the list
+            Answerlist.Clear();
+            //add the correct answers to the list
+            Answerlist.AddRange(CorrectAnswers);
+
+        }
+
+        private void CompareAnswers(List<string> AnswerList, List<string> TestList)
+        {
+            //  Initialize variables
+            int correctCount = 0;
+                List<int> incorrectQuestions = new List<int>();
+
+                for (int i = 0; i < AnswerList.Count; i++)
+                //loop through the list of answers
+                {
+                    if (i < TestList.Count && AnswerList[i] == TestList[i])
+                //  Compare the answers
+                {
+                    correctCount++;
+                    //  Increment the correct count
+                }
+                else
+                    {
+                        incorrectQuestions.Add(i + 1);
+                    //  Add the question number to the list of incorrect questions
+                }
+            }
+
+                // Determine pass or fail
+                bool passed = correctCount >= 15;
+                string resultMessage = passed ? "Passed" : "Failed";
+
+                // Display results
+                LabelResults.Text = ($@"
+            Result: {resultMessage}
+            Correct Answers: {correctCount}
+            Incorrect Answers: {incorrectQuestions.Count}
+            Incorrect Questions: {string.Join(", ", incorrectQuestions)}
+            ");
+            }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            //  Close the form
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            
+                List<string> AnswerList = new List<string>();
+            //  Create a list to hold the answers
+            List<string> TestList = new List<string>();
+            //  Create a list to hold the test answers
+            CorrectTest(AnswerList);
+            //  Get the correct answers
+            ReadTest(TestList);
+            //  Read the test answers
+            CompareAnswers(AnswerList, TestList);
+            //  Compare the answers
+        }
+    }
+}
